76f8d85c08aead8c3cb8c8db3b00594d31e07672
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2019-05-22 01:00:38 -0400

    second commit

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  private currentIndex$ = new BehaviorSubject(0);
+
   questions = [
     {
       question: 'Inside which HTML element do we put the JavaScript?',
@@ -11,7 +14,7 @@ export class DataService {
     },
     {
       question: 'Inside which HTML element do we put the JavaScript?',
-      options: ['scripting', 'js', 'script', 'javascript']
+      options: ['scripting test', 'js', 'script', 'javascript']
     },
     {
       question: 'Inside which HTML element do we put the JavaScript?',
@@ -27,7 +30,19 @@ export class DataService {
     }
   ];
 
+  getCurrentIndex() {
+    return this.currentIndex$.asObservable();
+  }
+
+  setCurrentIndex(index: any) {
+    this.currentIndex$.next(index);
+  }
+
   getQuestions() {
     return this.questions;
+  }
+
+  getQuestionOnIndex(index: number) {
+    return this.questions[index];
   }
 }
--- src/app/question-paper/question-paper.component.ts
+++ src/app/question-paper/question-paper.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { CurrencyIndex } from '@angular/common/src/i18n/locale_data';
 
 @Component({
   selector: 'app-question-paper',
@@ -8,16 +9,34 @@ import { DataService } from '../data.service';
 })
 export class QuestionPaperComponent implements OnInit {
   questions = [];
+  question: any;
+  currentIndex: number;
+  questionsLength: number;
+  disableNext = false;
   constructor(public dataService: DataService) {}
 
   ngOnInit() {
-    this.questions = this.dataService.getQuestions();
-    console.log(this.questions);
+    this.questionsLength = this.dataService.getQuestions().length - 1;
+    console.log(this.questionsLength);
+    // this.questions = this.dataService.getQuestions();
+    this.dataService.getCurrentIndex().subscribe(index => {
+      this.currentIndex = index;
+      this.question = this.dataService.getQuestionOnIndex(this.currentIndex);
+      if (this.currentIndex === this.questionsLength) {
+        this.disableNext = true;
+      } else {
+        this.disableNext = false;
+      }
+    });
   }
 
   createRange(number) {
     for (let i = 1; i <= number; i++) {
       this.questions.push(i);
     }
+  }
+
+  incrementIndex() {
+    this.dataService.setCurrentIndex(this.currentIndex + 1);
   }
 }
